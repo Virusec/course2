e7b91d1d283485b3e6c801fed16b66c418e77752
Anatoliy <t7313@ya.ru>
2022-01-06 09:06:35 +0300

    Initial commit

---
+++ src/test/java/pro/sky/javadev/course2/Course2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.javadev.course2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Course2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
